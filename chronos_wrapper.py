2b8a6f09b0fca1a8e84ddd03abb0c770ced04841
William A Rodriguez <william.a@alum.mit.edu>
2024-07-04 16:05:49 +0200

    Benchmark chronos against SARIMA and actual values

--- chronos_wrapper.py
+++ chronos_wrapper.py
@@ -6,9 +6,11 @@ import matplotlib.pyplot as plt
 import os
 import statsmodels.api as sm
 import pmdarima as pm
+import utils
 
 
 CHRONOS_NUM_SAMPLES_DEFAULT = 20
+DATE_COLUMN = 'Date'
 
 """
 
@@ -45,16 +47,18 @@ DEVICE_MAP = "cpu" # cuda, mps, cpu
 
 # use "cpu" for CPU inference and "mps" for Apple Silicon
 
-def chronos_predict(input_data: [float],
-            context_range: [int],
-            prediction_length: int,
-            autoregressions: int = 0,
-            median_predictions: [float] = pd.Series([]),
-            low_predictions: [float] = pd.Series([]),
-            high_predictions: [float] = pd.Series([]),
-            initial_context_start: int = None,
-            initial_context_end: int = None
-):
+def chronos_predict(
+  input_data: [float],
+  column: str,
+  context_range: [int],
+  prediction_length: int,
+  autoregressions: int = 0,
+  median_predictions: [float] = pd.Series([]),
+  low_predictions: [float] = pd.Series([]),
+  high_predictions: [float] = pd.Series([]),
+  initial_context_start: int = None,
+  initial_context_end: int = None
+  ):
     pipeline = ChronosPipeline.from_pretrained(
         "amazon/chronos-t5-small",
         device_map=DEVICE_MAP,  
@@ -83,6 +87,15 @@ def chronos_predict(input_data: [float],
     n = len(input_data)
     (context_start_index, context_end_index) = context_range
 
+    print("Data")
+    print(input_data)
+    if type(context_start_index) == str:
+      # convert dates to index
+      mask = (input_data[DATE_COLUMN] >= context_start_index) & (input_data[DATE_COLUMN] <= context_end_index)
+      training_df = input_data.loc[mask]
+      context_start_index = utils.find_first_occurrence_index(input_data, context_start_index,DATE_COLUMN)
+      context_end_index = utils.find_first_occurrence_index(input_data, context_end_index,DATE_COLUMN)    
+
     if n < 2:
         raise ValueError("Time series data has only one data point or is empty")
     elif not 0 <= context_start_index < context_end_index <= (n + len(median_predictions)):
@@ -91,7 +104,7 @@ def chronos_predict(input_data: [float],
     # Convert data to tensor
     # print ("context_slice", type(context_slice), context_slice)
 
-    extended_slice_by_predictions = pd.concat([input_data, median_predictions])
+    extended_slice_by_predictions = pd.concat([input_data[column], median_predictions])
     context_slice = extended_slice_by_predictions[context_start_index:context_end_index]
 
     context_data_tensor = torch.tensor(context_slice.tolist(), dtype=torch.float32)
@@ -111,8 +124,9 @@ def chronos_predict(input_data: [float],
     low_predictions = pd.concat([low_predictions,pd.Series(low)])
     high_predictions = pd.concat([high_predictions,pd.Series(high)])   
 
-    initial_context_start_to_carry = context_range[0] if initial_context_start is None else initial_context_start
-    initial_context_end_to_carry = context_range[-1] if initial_context_end is None else initial_context_end
+    # context_start_index in two lines below will need to be amended for recursion
+    initial_context_start_to_carry = context_start_index if initial_context_start is None else initial_context_start
+    initial_context_end_to_carry = context_end_index if initial_context_end is None else initial_context_end
 
     if autoregressions == 0:
         # graph
@@ -120,11 +134,12 @@ def chronos_predict(input_data: [float],
 
         num_median_predictions = len(median_predictions)
         
-
+        # print("input_data[column][:initial_context_start_to_carry],")
+        print(input_data[column][:initial_context_start_to_carry],)
         plt.figure(figsize=(8, 4))
-        plt.plot(range(initial_context_start_to_carry), input_data[:initial_context_start_to_carry], color="royalblue", label="Reference data")
-        plt.plot(range(initial_context_start_to_carry, initial_context_end_to_carry,), input_data[initial_context_start_to_carry:initial_context_end_to_carry], color="green", label="Context data")
-        plt.plot(range(initial_context_end_to_carry,n), input_data[initial_context_end_to_carry:], color="royalblue", label="Reference data")
+        plt.plot(range(initial_context_start_to_carry), input_data[column][:initial_context_start_to_carry], color="royalblue", label="Reference data")
+        plt.plot(range(initial_context_start_to_carry, initial_context_end_to_carry,), input_data[column][initial_context_start_to_carry:initial_context_end_to_carry], color="green", label="Context data")
+        plt.plot(range(initial_context_end_to_carry,n), input_data[column][initial_context_end_to_carry:], color="royalblue", label="Reference data")
         plt.plot(range(initial_context_end_to_carry, initial_context_end_to_carry + num_median_predictions), median_predictions, color="tomato", label="Median forecast")
         plt.fill_between(range(initial_context_end_to_carry, initial_context_end_to_carry + num_median_predictions), low_predictions, high_predictions, color="tomato", alpha=0.3, label="80% prediction interval")
         plt.legend()
@@ -141,6 +156,7 @@ def chronos_predict(input_data: [float],
         print("RECURSING: Autoregressions left:", autoregressions - 1)
         return chronos_predict(
           input_data,
+          column,
           new_context_range,
           prediction_length,
           autoregressions - 1,
---
+++ sarima_wrapper.py
@@ -0,0 +1,70 @@
+# S-ARIMA prediction model
+import pmdarima as pm
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from statsmodels.tsa.seasonal import seasonal_decompose
+import statsmodels.api as sm
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+import utils
+import matplotlib.pyplot as plt
+
+
+DATE_COLUMN = "Date"
+
+def sarima_predict(df, column, training_start_index,training_end_index, prediction_length):
+  # Assuming df is your DataFrame and it has been previously defined
+  training_df = None
+
+  if type(training_start_index) == str:
+    # convert dates to index
+    mask = (df[DATE_COLUMN] >= training_start_index) & (df[DATE_COLUMN] <= training_end_index)
+    training_df = df.loc[mask]
+    training_start_index = utils.find_first_occurrence_index(df, training_start_index,DATE_COLUMN)
+    training_end_index = utils.find_first_occurrence_index(df, training_end_index,DATE_COLUMN)
+    forecast_index = range(training_end_index + 1,training_end_index + 1 + prediction_length)
+  elif not training_start_index is None and not training_end_index is None:
+    training_df = df[training_start_index:training_end_index]
+    forecast_index = df.index[training_end_index:training_end_index + prediction_length]
+  else:
+    raise ValueError(f'training_start_index is {type(training_start_index)}. Must be int or str.')
+
+  # Use auto_arima to find the optimal p, d, q, P, D, Q, m values
+  stepwise_fit = pm.auto_arima(training_df[column],
+                              start_p=0, start_q=0, max_p=5, max_q=5,
+                              start_P=0, start_Q=0, max_P=5, max_Q=5,
+                              seasonal=True, m=12,  # Assuming monthly seasonal data, adjust m as needed
+                              trace=True,
+                              error_action='ignore',
+                              suppress_warnings=True,
+                              stepwise=True)
+
+  # Extract optimal parameters
+  optimal_order = stepwise_fit.order
+  optimal_seasonal_order = stepwise_fit.seasonal_order
+  p, d, q = optimal_order
+  P, D, Q, m = optimal_seasonal_order
+
+  print(f'Optimal order: p={p}, d={d}, q={q}')
+  print(f'Optimal seasonal order: P={P}, D={D}, Q={Q}, m={m}')
+
+  # Fit the SARIMA model with the optimal order
+  model = sm.tsa.statespace.SARIMAX(training_df[column],
+                                    order=(p, d, q),
+                                    seasonal_order=(P, D, Q, m))
+  model_fit = model.fit()
+  # Forecast
+
+  comparison_values = df[forecast_index[0]:forecast_index[-1]][column]
+
+  sarima_forecast = model_fit.forecast(steps=prediction_length)
+
+
+
+  # Plot the original data and the forecast
+  plt.figure(figsize=(8, 4))
+  plt.plot(df[column][training_start_index: forecast_index[-1]], color="royalblue", label="historical data")
+  plt.plot(forecast_index, sarima_forecast, color="tomato", label="median forecast")
+  plt.legend()
+  plt.show()
+
+  return sarima_forecast
