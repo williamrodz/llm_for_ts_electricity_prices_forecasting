56cb1971ed78dbb69b20028602b43c725d6658c4
William A Rodriguez <william.a@alum.mit.edu>
2024-07-20 12:06:44 +0100

    GP: Normallized values to zero and unit variance

--- gp_wrapper.py
+++ gp_wrapper.py
@@ -12,22 +12,31 @@ def gp_predict(df, column, date_column, training_start_index, training_end_index
 
   # Extract the data
   training_df = df[training_start_index:training_end_index]
-  length_scale = np.mean(training_df[column].values) * 1
+  
+  # Move the training data to zero mean
+  mean = training_df[column].mean()
+  std = training_df[column].std()
+
+  if std == 0:
+    raise ValueError("Standard deviation is zero, cannot normalize.")
+  
+  normalized_training_data = (training_df[column] - mean) / std
 
   # Define the kernel
   kernel = RBF()
 
   # Create Gaussian Process Regressor
   gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=10)
-
   train_data_time_steps_array = np.array(range(training_start_index,training_end_index)).reshape(-1, 1)
-  training_y_values_array = np.array(training_df[column])
+
   # Fit to the training data
-  gp.fit(train_data_time_steps_array, training_y_values_array)
+  gp.fit(train_data_time_steps_array, normalized_training_data)
 
   # Make predictions
   forecast_index = np.array(range(train_data_time_steps_array[-1][0], train_data_time_steps_array[-1][0] + prediction_length))
   y_pred, sigma = gp.predict(forecast_index.reshape(-1, 1), return_std=True)
+
+  y_pred_rescaled = y_pred * std + mean
 
   actual_values = df[forecast_index[0]:forecast_index[-1]][column]
 
@@ -42,9 +51,9 @@ def gp_predict(df, column, date_column, training_start_index, training_end_index
     plt.plot(df[column][left_most_index: training_start_index], color="royalblue", label="Historical Data")
     plt.plot(df[column][training_start_index: training_end_index], color="green", label="Context")
     plt.plot(df[column][forecast_index[0]: right_most_index], color="royalblue", label="Post Context Historical Data")
-    plt.plot(forecast_index, y_pred, color="tomato", label="GP Forecast")
+    plt.plot(forecast_index, y_pred_rescaled, color="tomato", label="GP Forecast")
     plt.legend()
     plt.show()
 
-  return y_pred
+  return y_pred_rescaled
 
